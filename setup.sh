334061d9c899c499f7e4990675eb8d0455c538f3
Chris Bensch <chris.bensch@gmail.com>
2014-05-27 21:00:44 +0900

    Updated setup.sh, made just a bit smarter
    In case setup.sh is ran more than once, these edits prevent duplicate
    (hopefully) entries of our repos while keeping any custom user entered
    repos.  Also creates new data storage directory “/root/data/discover”.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## Variables
+user=$(whoami)
+
 clear
 echo
 echo
@@ -18,18 +21,45 @@ echo -e "\e[1;33mInstalling xdotool.\e[0m"
 apt-get -y install xdotool
 echo
 echo -e "\e[1;33mUpdating repositories.\e[0m"
-echo "# Regular repos" > /etc/apt/sources.list
-echo "deb http://http.kali.org/kali kali main non-free contrib" >> /etc/apt/sources.list
-echo "deb http://security.kali.org/kali-security kali/updates main contrib non-free" >> /etc/apt/sources.list
-echo >> /etc/apt/sources.list
 
-echo "# Source repos" >> /etc/apt/sources.list
-echo "deb-src http://http.kali.org/kali kali main non-free contrib" >> /etc/apt/sources.list
-echo "deb-src http://security.kali.org/kali-security kali/updates main contrib non-free" >> /etc/apt/sources.list
-echo >> /etc/apt/sources.list
+## Backup sources.list
+cp -p /etc/apt/sources.list /etc/apt/sources.list.before.setup.bak
 
-echo "# Bleeding Edge repos" >> /etc/apt/sources.list
-echo "deb http://repo.kali.org/kali kali-bleeding-edge main" >> /etc/apt/sources.list
+## Remove all previous kali.org repo lines as well as ours in case of multiple times running setup
+sed -i '/Regular repos/d' /etc/apt/sources.list
+sed -i '/Source repos/d' /etc/apt/sources.list
+sed -i '/Bleeding Edge repos/d' /etc/apt/sources.list
+sed -i '/kali.org/d' /etc/apt/sources.list
+
+## Add remaining sources from sources.list after cleaning to temp file
+cat /etc/apt/sources.list > /tmp/sources.leftover
+
+## Add our repo lines to new temp file
+echo > /tmp/sources.discoversetup
+echo "# Regular repos" >> /tmp/sources.discoversetup
+echo "deb http://repo.kali.org/kali kali main non-free contrib" >> /tmp/sources.discoversetup
+echo "deb http://security.kali.org/kali-security kali/updates main contrib non-free" >> /tmp/sources.discoversetup
+echo >> /tmp/sources.discoversetup
+
+echo "# Source repos" >> /tmp/sources.discoversetup
+echo "deb-src http://repo.kali.org/kali kali main non-free contrib" >> /tmp/sources.discoversetup
+echo "deb-src http://security.kali.org/kali-security kali/updates main contrib non-free" >> /tmp/sources.discoversetup
+echo >> /tmp/sources.discoversetup
+
+echo "# Bleeding Edge repos" >> /tmp/sources.discoversetup
+echo "deb http://repo.kali.org/kali kali-bleeding-edge main" >> /tmp/sources.discoversetup
+echo >> /tmp/sources.discoversetup
+
+## Cut empty lines from top and merge file contents
+sed -i '/./,$!d' /tmp/sources.leftover
+olddata=$(cat /tmp/sources.leftover)
+newdata=$(cat /tmp/sources.discoversetup)
+echo -en "$newdata\n\n$olddata\n" > /etc/apt/sources.list
 echo
+echo
+
+## Create data directory
+echo -e "\e[1;33mCreating Data Directory.\e[0m"
+mkdir -p /$user/data/discover
 echo
 
