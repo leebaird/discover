88add15cdad81ffb08c04f0e30b579164b2250bb
Jason Soto <jsitech@live.com>
2015-09-04 17:27:47 -0500

    Goofile verification and install in Setup Script

--- setup.sh
+++ setup.sh
@@ -14,7 +14,16 @@ echo -e "\e[1;33mInstalling xdotool.\e[0m"
 apt-get -y install xdotool
 echo
 echo
-echo -e "\e[1;33mInstalling goofile.\e[0m"
+echo -e "\e[1;33mChecking if goofile is installed, if not installing.\e[0m"
 echo
-apt-get -y install goofile
+
+which goofile >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo ""
+else
+    echo ""
+    apt-get -y install goofile
+    echo ""
+    exit 1
+fi
 echo
