ff8927ee7b376644babea11942e87f431ebf1a36
b3nac <rockapparatus@yahoo.com>
2015-09-16 06:49:46 +0000

    Added Arch support

--- setup.sh
+++ setup.sh
@@ -5,27 +5,51 @@ echo
 echo
 
 echo -e "\e[1;33mInstalling Filezilla.\e[0m"
-apt-get -y install filezilla
+if [ -n "$(command -v apt-get)" ]; then
+    apt-get -y install filezilla
+
+elif [ -n "$(command -v pacman)" ]; then
+	pacman -S filezilla
+else
+	echo "apt-get and pacman aren't package managers?"
+	exit 1;
+fi
 echo
 echo -e "\e[1;33mInstalling gedit.\e[0m"
-apt-get -y install gedit
+if [ -n "$(command -v apt-get)" ]; then
+   apt-get -y install gedit
+
+elif [ -n "$(command -v pacman)" ]; then
+   pacman -S gedit
+   
+fi
 echo
 echo -e "\e[1;33mInstalling xdotool.\e[0m"
-apt-get -y install xdotool
-echo
+if [ -n "$(command -v apt-get)" ]; then
+   apt-get -y install xdotool
+
+elif [ -n "$(command -v pacman)" ]; then
+	pacman -S xdotool
+	
+fi
 echo
 echo -e "\e[1;33mChecking if goofile is installed, if not installing.\e[0m"
-echo
 
 which goofile >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
      echo
-else
+elif [ -n "$(command -v apt-get)" ]; then
      echo
      apt-get -y install goofile
      echo
      exit 1
+elif [ -n "$(command -v pacman)" ]; then
+	echo
+     pacman -S goofile
+     echo
+     exit 1
+     
 fi
 
 echo
