52fc2604fe0823e86f5b4a3cd940cc68f0a4cc80
b3nac <rockapparatus@yahoo.com>
2015-09-19 18:00:50 +0000

    fixed indentation.

--- setup.sh
+++ setup.sh
@@ -5,33 +5,37 @@ echo
 echo
 
 echo -e "\e[1;33mInstalling Filezilla.\e[0m"
+
 if [ -n "$(command -v apt-get)" ]; then
     apt-get -y install filezilla
 
 elif [ -n "$(command -v pacman)" ]; then
-	pacman -S filezilla
+    pacman -S filezilla
 else
-	echo "apt-get and pacman aren't package managers?"
-	exit 1;
+    echo "apt-get and pacman aren't package managers?"
+    exit 1;
 fi
+
 echo
 echo -e "\e[1;33mInstalling gedit.\e[0m"
+
 if [ -n "$(command -v apt-get)" ]; then
-   apt-get -y install gedit
+    apt-get -y install gedit
 
 elif [ -n "$(command -v pacman)" ]; then
-   pacman -S gedit
-   
+    pacman -S gedit   
 fi
+
 echo
 echo -e "\e[1;33mInstalling xdotool.\e[0m"
+
 if [ -n "$(command -v apt-get)" ]; then
-   apt-get -y install xdotool
+    apt-get -y install xdotool
 
 elif [ -n "$(command -v pacman)" ]; then
-	pacman -S xdotool
-	
+    pacman -S xdotool	
 fi
+
 echo
 echo -e "\e[1;33mChecking if goofile is installed, if not installing.\e[0m"
 
@@ -49,7 +53,6 @@ elif [ -n "$(command -v pacman)" ]; then
      pacman -S goofile
      echo
      exit 1
-     
 fi
 
 echo
