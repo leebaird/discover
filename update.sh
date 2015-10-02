417fdb2fd1e8292f0eb8612f08182a38a8393475
Lee <leebaird@gmail.com>
2015-10-02 19:35:21 -0400

    Tweaked update script.

--- update.sh
+++ update.sh
@@ -8,13 +8,14 @@ x=$(uname -a | awk '{print $3}' | cut -d '.' -f1)
 
 if [[ $x > 3 ]]; then
      echo -e "\e[1;34mUpdating Kali 2.0.\e[0m"
-     apt-get update ; apt-get -y upgrade
+     apt-get update ; apt-get -y upgrade ; echo
 
      if [ ! -d /usr/bin/goofile ]; then
           echo -e "\e[1;33mInstalling goofile.\e[0m"
           wget "https://goofile.googlecode.com/files/goofilev1.5.zip"
           unzip -p goofilev1.5.zip goofilev1.5/goofile.py > /usr/bin/goofile
           chmod +x /usr/bin/goofile
+          echo
      fi
 
      if [ -d /opt/discover/.git ]; then
