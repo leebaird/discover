62f8fe057b921bf0a1d46109e96b72b935a667c7
L1ghtn1ng <townsend891@hotmail.com>
2016-08-14 18:24:49 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
+
+setup(
+    name='Discover',
+    version='2.0-dev',
+    url='http://github.com/leebaird/discover/',
+    license='MIT',
+    author='Lee Baird',
+    author_email='leebaird@gmail.com',
+    zip_safe=False,
+    install_requires=[
+        'shodan>=1.5.5',
+        'dnspython>=1.14.0',
+    ])
