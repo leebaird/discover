080dc1d0bff37b4756c09313143aa9ae25927418
L1ghtn1ng <townsend891@hotmail.com>
2016-08-14 17:33:46 +0100

    Foundation laying

---
+++ discover.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+from src.core.discovercore import *
+from src.core.menus import *
+
+
+
+
+
+Discover.banner()
---
+++ setup.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
