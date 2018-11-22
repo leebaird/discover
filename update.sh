0da5f16f150795bcc7b8737d9f4fd59d6b921a99
Arthur Kay <arthurakay@gmail.com>
2018-11-22 11:33:51 -0600

    run pip install from the correct location

--- update.sh
+++ update.sh
@@ -235,6 +235,7 @@ fi
 echo -e "${BLUE}Updating locate database.${NC}" ; updatedb
 
 echo -e "${BLUE}Updating Python dependencies (via pip)${NC}"
+cd /opt/discover
 pip install -r requirements.txt
 
 echo
