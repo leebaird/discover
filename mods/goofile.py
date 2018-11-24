22753283b5c0bd9a2b96e30cf5a9a8e1d0d53c0e
Arthur Kay <arthurakay@gmail.com>
2018-11-24 13:13:54 -0600

    clickable links for formatting

--- mods/goofile.py
+++ mods/goofile.py
@@ -9,12 +9,17 @@ domain = sys.argv[1]
 filetype = sys.argv[2]
 start = 0
 results = []
+totalFiles = 0
+
+linkTemplate = '<li><a href="{0}" target="_blank">{0}</a></li>'
 
 def googleDork():
     global domain
     global filetype
     global start
     global results
+    global totalFiles
+    global linkTemplate
 
     url = 'https://google.com/search?num=500&q=site:{0}+filetype:{1}&num=100&start={2}'.format(domain, filetype, start)
     page = requests.get(url)
@@ -22,26 +27,35 @@ def googleDork():
 
     results = tree.xpath('//*[@class="r"]/a/@href')
 
+    totalFiles += len(results)
+
     for link in results:
         m = re.match('^\/url\?q=(.*)\&sa', link)
         if m:
-            print(m.groups()[0])
+            print(linkTemplate.format(m.groups()[0]))
         else:
-            print('Could not parse the following link: ' + link)
+            print('<li>Could not parse: {0}</li>'.format(link))
 
     if results != []:
         start += 100
 
 def main():
     global results
+    global totalFiles
+    global filetype
+
+    print('<ul>')
 
     googleDork()
 
     if results == []:
-        print("No results were found.")
+        print("<li>No results were found.</li>")
         sys.exit()
 
     while results != []:
         googleDork()
+
+    print('</ul>')
+    print('<p>Total number of {0} files: {1}</p>'.format(filetype.upper(), totalFiles))
 
 main()
