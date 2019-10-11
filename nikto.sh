575211d6be8a6365365aa8242f64eb1a267660a5
Lee Baird <leebaird@gmail.com>
2019-10-11 01:36:24 -0500

    Created new file - nikto.sh

---
+++ nikto.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+f_runlocally
+clear
+f_banner
+
+echo -e "${BLUE}Run multiple instances of Nikto in parallel.${NC}"
+echo
+echo "1.  List of IPs."
+echo "2.  List of IP:port."
+echo "3.  Previous menu"
+echo
+echo -n "Choice: "
+read choice
+
+case $choice in
+     1)
+     f_location
+
+     echo
+     echo -n "Port (default 80): "
+     read port
+     echo
+
+     # Check if port is a number
+     echo "$port" | grep -E "^[0-9]+$" 2>/dev/null
+     isnum=$?
+
+     if [ $isnum -ne 0 ] && [ ${#port} -gt 0 ]; then
+          f_error
+     fi
+
+     if [ ${#port} -eq 0 ]; then
+          port=80
+     fi
+
+     if [ $port -lt 1 ] || [ $port -gt 65535 ]; then
+          f_error
+     fi
+
+     mkdir $home/data/nikto-$port
+
+     while read -r line; do
+          xdotool key ctrl+shift+t
+          xdotool type "nikto -h $line -port $port -Format htm --output $home/data/nikto-$port/$line.htm ; exit"
+          sleep 1
+          xdotool key Return
+     done < "$location"
+     ;;
+
+     2)
+     f_location
+
+     mkdir $home/data/nikto
+
+     while IFS=: read -r host port; do
+          xdotool key ctrl+shift+t
+          sleep 1
+          xdotool type "nikto -h $host -port $port -Format htm --output $home/data/nikto/$host-$port.htm ; exit"
+          sleep 1
+          xdotool key Return
+     done < "$location"
+     ;;
+
+     3) f_main;;
+     *) f_error;;
+esac
+
+echo
+echo $medium
+echo
+echo "***Scan complete.***"
+echo
+echo
+echo -e "The new report is located at ${YELLOW}$home/data/nikto/${NC}\n"
+echo
+echo
+exit
