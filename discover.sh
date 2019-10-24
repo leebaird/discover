6bb56e5def3aeb1bb60253dbb8d8572fb731c297
Lee Baird <leebaird@gmail.com>
2019-10-24 13:32:36 -0500

    Refactored multitabls.

--- discover.sh
+++ discover.sh
@@ -2457,103 +2457,6 @@ exit
 
 ##############################################################################################################
 
-f_multitabs(){
-f_runlocally
-clear
-f_banner
-
-echo -e "${BLUE}Open multiple tabs in Firefox with:${NC}"
-echo
-echo "1.  List"
-echo "2.  Directories from robots.txt."
-echo "3.  Previous menu"
-echo
-echo -n "Choice: "
-read choice
-
-case $choice in
-     1) f_location
-     echo -n "Use SSL? (y/N) "
-     read ssl
-
-     $web &
-     sleep 2
-
-     if [ -z $ssl ]; then
-          for i in $(cat $location); do
-               $web http://$i &
-               sleep 1
-          done
-     elif [ "$ssl" == "y" ]; then
-          for i in $(cat $location); do
-               $web https://$i &
-               sleep 1
-          done
-     else
-          f_error
-     fi
-     ;;
-
-     2)
-     echo
-     echo $medium
-     echo
-     echo "Usage: target.com or target-IP"
-     echo
-     echo -n "Domain: "
-     read domain
-
-     # Check for no answer
-     if [[ -z $domain ]]; then
-          f_error
-     fi
-
-     wget -q $domain/robots.txt
-
-     # Check if the file is empty
-     if [ ! -s robots.txt ]; then
-          echo
-          echo -e "${RED}$medium${NC}"
-          echo
-          echo -e "${RED}                          *** No robots file discovered. ***${NC}"
-          echo
-          echo -e "${RED}$medium${NC}"
-          sleep 2
-          f_main
-     fi
-
-     grep 'Disallow' robots.txt | awk '{print $2}' > tmp
-
-     $web &
-     sleep 2
-
-     for i in $(cat tmp); do
-          $web http://$domain$i &
-          sleep 1
-     done
-
-     rm robots.txt
-     mv tmp $home/data/$domain-robots.txt
-
-     echo
-     echo $medium
-     echo
-     echo "***Scan complete.***"
-     echo
-     echo
-     echo -e "The new report is located at ${YELLOW}$home/data/$domain-robots.txt${NC}\n"
-     echo
-     echo
-     exit
-     ;;
-
-     3) f_main;;
-     *) f_error;;
-esac
-}
-
-##############################################################################################################
-
 f_main(){
 clear
 f_banner
@@ -2600,7 +2503,7 @@ case $choice in
      7) f_single;;
      8) f_enumerate;;
      9) f_directObjectRef;;
-     10) f_multitabs;;
+     10) $discover/multitabs.sh && exit;;
      11) $discover/nikto.sh && exit;;
      12) $discover/ssl.sh && exit;;
      13) $discover/parse.sh && exit;;
