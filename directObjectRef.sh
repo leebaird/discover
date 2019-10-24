ecaf9186667c756e764ea5fa15b5cc52675f9411
Lee Baird <leebaird@gmail.com>
2019-10-24 13:45:06 -0500

    Forgot to add new file.

---
+++ directObjectRef.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear
+f_banner
+
+echo -e "${BLUE}Using Burp, authenticate to a site, map & Spider, then log out.${NC}"
+echo -e "${BLUE}Target > Site map > select the URL > right click > Copy URLs in${NC}"
+echo -e "${BLUE}this host. Paste the results into a new file.${NC}"
+
+f_location
+
+for i in $(cat $location); do
+     curl -sk -w "%{http_code} - %{url_effective} \\n" "$i" -o /dev/null 2>&1 | tee -a tmp
+done
+
+cat tmp | sort -u > DirectObjectRef.txt
+mv DirectObjectRef.txt $home/data/DirectObjectRef.txt
+rm tmp
+
+echo
+echo $medium
+echo
+echo "***Scan complete.***"
+echo
+echo
+echo -e "The new report is located at ${YELLOW}$home/data/DirectObjectRef.txt${NC}\n"
+echo
+echo
+
