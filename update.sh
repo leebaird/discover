db720ed4aad1e707e55b963171c96aa6669d9c4d
Lee Baird <leebaird@gmail.com>
2021-02-26 10:11:53 -0500

    Added Go notes and changed repos back to Cobbr for Covenant.

--- update.sh
+++ update.sh
@@ -102,7 +102,7 @@ if [ -d /opt/Covenant/.git ]; then
      echo
 else
      echo -e "${YELLOW}Installing Covenant.${NC}"
-     git clone --recurse-submodules https://github.com/ZeroPointSecurity/Covenant.git /opt/Covenant
+     git clone --recurse-submodules https://github.com/cobbr/Covenant /opt/Covenant
      wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
      dpkg -i packages-microsoft-prod.deb
      apt update
