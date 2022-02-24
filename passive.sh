9338a692a9ac00a7be475f0d899ef47b2f018269
Lee Baird <leebaird@gmail.com>
2022-02-23 19:10:22 -0600

    Jay fixed DNSrecon. Reverted back to repo version.

--- passive.sh
+++ passive.sh
@@ -153,8 +153,7 @@ echo
 ###############################################################################################################################
 
 echo "DNSRecon                  (5/$total)"
-dnsrecon -d $domain -n 8.8.8.8 -t std > tmp
-#python3 /opt/DNSRecon/dnsrecon.py -d $domain -n 8.8.8.8 -t std > tmp
+python3 /opt/DNSRecon/dnsrecon.py -d $domain -n 8.8.8.8 -t std > tmp
 cat tmp | egrep -v '(All queries will|Could not|DNSSEC|Error|It is resolving|Performing|Records|Recursion|TXT|Version|Wildcard resolution)' | sed 's/\[\*\]//g; s/\[+\]//g; s/^[ \t]*//' | column -t | sort | sed 's/[ \t]*$//' > records
 cat tmp | grep 'TXT' | sed 's/\[\*\]//g; s/\[+\]//g; s/^[ \t]*//' | sort | sed 's/[ \t]*$//' >> records
 
