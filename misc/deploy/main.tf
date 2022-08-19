e4aaf86d5961c5790b3eeba6e7b04dfe3653b6a5
Lee Baird <leebaird@gmail.com>
2022-08-19 19:57:15 -0400

    Updated fw rules.

--- misc/deploy/main.tf
+++ misc/deploy/main.tf
@@ -55,25 +55,25 @@ resource "aws_security_group" "operators" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["<home IP>"]
+    cidr_blocks = ["<operator IPs>"]
   }
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["<home IP>"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["<home IP>"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 53
     to_port     = 53
     protocol    = "udp"
-    cidr_blocks = ["<home IP>"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "Operators"
