6d97717c56ccfc413141f7244f7b067726e59374
Lee Baird <leebaird@gmail.com>
2023-08-08 16:57:50 -0500

    Modified Havoc install.

--- update.sh
+++ update.sh
@@ -295,8 +295,13 @@ if [ -d /opt/Havoc/.git ]; then
      echo
 else
      echo -e "${YELLOW}Installing Havoc.${NC}"
+     apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev python3-dev libboost-all-dev mingw-w64 nasm
      git clone https://github.com/HavocFramework/Havoc /opt/Havoc
-     apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev libspdlog-dev python3-dev libboost-all-dev mingw-w64 nasm
+     cd /opt/Havoc/teamserver/
+     go mod download golang.org/x/sys
+     go mod download github.com/ugorji/go
+     cd ..
+     make all
      echo
 fi
 
