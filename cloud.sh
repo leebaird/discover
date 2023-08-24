2615eebec91fd55eacd4c3d44184a8222fa6309f
Lee Baird <leebaird@gmail.com>
2023-08-24 16:11:38 -0500

    Added cloud.sh to check for spillage.

---
+++ cloud.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo
+echo "Seach cloud providers for spillage."
+echo
+echo -n "Domain:  "
+read domain
+
+# Check for no answer
+if [ -z $domain ]; then
+     echo
+     echo "[!] No domain entered."
+     echo
+     exit 1
+fi
+
+xdg-open https://www.google.com/search?q=site:http://s3.amazonaws.com+%22$domain%22 &
+sleep 4
+xdg-open https://www.google.com/search?q=site:http://blob.core.windows.net+%22$domain%22 &
+sleep 5
+xdg-open https://www.google.com/search?q=site:http://googleapis.com+%22$domain%22 &
+sleep 6
+xdg-open https://www.google.com/search?q=site:http://drive.google.com+%22$domain%22 &
