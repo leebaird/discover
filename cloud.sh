3afebf1b4e6951c61f8e99773100209984c14928
Lee Baird <leebaird@gmail.com>
2023-09-01 20:23:59 -0500

    Added script to help search for CVEs.

--- cloud.sh
+++ cloud.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# by Lee Baird (@discoverscripts)
+
 echo
 echo "Seach cloud providers for spillage."
 echo
---
+++ cve.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# by Lee Baird (@discoverscripts)
+
+echo
+echo "Use a CVE to search for exploits."
+echo
+echo -n "CVE:  "
+read cve
+
+# Check for no answer
+if [ -z $cve ]; then
+     echo
+     echo "[!] No CVE entered."
+     echo
+     exit 1
+fi
+
+xdg-open https://github.com/search?q=$cve&type=repositories &
+sleep 4
+xdg-open https://www.google.com/search?q=%22$cve%22+AND+exploit &
+sleep 4
+xdg-open https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query=$cve&queryType=phrase&search_type=all&isCpeNameSearch=false &
+sleep 4
+xdg-open https://www.rapid7.com/db/?q=$cve&type=nexpose &
+sleep 4
+xdg-open https://www.tenable.com/plugins &
