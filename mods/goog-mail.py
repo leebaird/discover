1a380a824917e6f5aa27192d54da300ab6ca4561
Lee Baird <leebaird@gmail.com>
2024-06-03 19:57:17 -0400

    Modified goog-mail.

--- mods/goog-mail.py
+++ mods/goog-mail.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 
-import http.client
 import re
-import string
 import sys
 import urllib.request
 import urllib.parse
 
 
-def StripTags(text):
-    finished = 0
+def strip_tags(text):
+    finished = False
     while not finished:
-        finished = 1
+        finished = True
         start = text.find(b"<")
         if start >= 0:
             stop = text[start:].find(b">")
             if stop >= 0:
                 text = text[:start] + text[start+stop+1:]
-                finished = 0
+                finished = False
     return text
 
 
@@ -32,16 +30,15 @@ page_counter = 0
 
 try:
     while page_counter < 50:
-        results = 'https://groups.google.com/groups?q='+str(domain_name)+'&hl=en&lr=&ie=UTF-8&start=' + repr(page_counter) + '&sa=N'
+        results = 'https://groups.google.com/groups?q=' + urllib.parse.quote(domain_name) + '&hl=en&lr=&ie=UTF-8&start=' + str(page_counter) + '&sa=N'
         request = urllib.request.Request(results)
         request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)')
-        opener = urllib.request.build_opener()                           
+        opener = urllib.request.build_opener()
         text = opener.open(request).read()
-        emails = re.findall(rb"([\w\.\-]+@'+domain_name+')", StripTags(text))
+        emails = re.findall(rb"[\w\.\-]+@" + re.escape(domain_name.encode()), strip_tags(text))
         for email in emails:
-            d[email] = 1
-            uniq_emails = list(d.keys())
-        page_counter = page_counter + 10
+            d[email.lower()] = 1
+        page_counter += 10
 except IOError as e:
     print(e)
 
@@ -49,18 +46,17 @@ page_counter_web = 0
 
 try:
     while page_counter_web < 50:
-        results_web = 'https://www.google.com/search?q=%40'+str(domain_name)+'&hl=en&lr=&ie=UTF-8&start=' + repr(page_counter_web) + '&sa=N'
+        results_web = 'https://www.google.com/search?q=%40' + urllib.parse.quote(domain_name) + '&hl=en&lr=&ie=UTF-8&start=' + str(page_counter_web) + '&sa=N'
         request_web = urllib.request.Request(results_web)
         request_web.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)')
-        opener_web = urllib.request.build_opener()                           
+        opener_web = urllib.request.build_opener()
         text = opener_web.open(request_web).read()
-        emails_web = re.findall(rb"([\w\.\-]+@'+domain_name+')", StripTags(text))
+        emails_web = re.findall(rb"[\w\.\-]+@" + re.escape(domain_name.encode()), strip_tags(text))
         for email_web in emails_web:
-            d[email_web] = 1
-            uniq_emails_web = list(d.keys())
-        page_counter_web = page_counter_web + 10
+            d[email_web.lower()] = 1
+        page_counter_web += 10
 except IOError as e:
     print(e)
 
-for uniq_emails_web in list(d.keys()):
-    print(uniq_emails_web+"")
+for uniq_email in d.keys():
+    print(uniq_email.decode('utf-8'))
