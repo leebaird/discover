ff7a7ff28c6d5748ed99fd6e4ce400555a442410
Lee Baird <leebaird@gmail.com>
2024-08-29 16:03:05 -0400

    Code review of nikto.

--- nikto.sh
+++ nikto.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-# Check for root
-if [ $EUID -ne 0 ]; then
+# Make sure not root
+if [ "$EUID" == 0 ]; then
+    echo
     echo
     echo "[!] This option cannot be ran as root."
     echo
-    exit
+    exit   # Do not use exit 1, in this case it will break the script
 fi
 
 clear
@@ -18,65 +19,63 @@ echo "2.  List of IP:port"
 echo "3.  Previous menu"
 echo
 echo -n "Choice: "
-read choice
+read -r choice
 
-case $choice in
+case "$choice" in
     1)
-    f_location
+        f_location
 
-    echo
-    echo -n "Port (default 80): "
-    read port
-    echo
+        echo
+        echo -n "Port (default 80): "
+        read -r port
+        echo
 
-    # Check if port is a number
-    echo "$port" | grep -E "^[0-9]+$" 2>/dev/null
-    isnum=$?
+        # Set default port to 80 if not provided
+        if [ -z "$port" ]; then
+            port=80
+        fi
 
-    if [ $isnum -ne 0 ] && [ ${#port} -gt 0 ]; then
-        f_error
-    fi
+        # Validate port
+        if ! [[ "$port" =~ ^[0-9]+$ ]] || [ "$port" -lt 1 ] || [ "$port" -gt 65535 ]; then
+            f_error
+        fi
 
-    if [ ${#port} -eq 0 ]; then
-        port=80
-    fi
+        mkdir -p "$HOME/data/nikto-$port"
 
-    if [ $port -lt 1 ] || [ $port -gt 65535 ]; then
-        f_error
-    fi
-
-    mkdir $home/data/nikto-$port
-
-    while read -r line; do
-        xdotool key ctrl+shift+t
-        xdotool type "nikto -h $line -port $port -no404 -maxtime 15m -Format htm --output $home/data/nikto-$port/$line.htm ; exit"
-        sleep 1
-        xdotool key Return
-    done < "$location"
-    ;;
+        while IFS= read -r line; do
+            xdotool key ctrl+shift+t
+            xdotool type "nikto -h $line -port $port -no404 -maxtime 15m -Format htm --output $HOME/data/nikto-$port/$line.htm ; exit"
+            sleep 1
+            xdotool key Return
+        done < "$location"
+        ;;
 
     2)
-    f_location
+        f_location
 
-    mkdir $home/data/nikto
+        mkdir -p "$HOME/data/nikto"
 
-    while IFS=: read -r host port; do
-        xdotool key ctrl+shift+t
-        sleep 1
-        xdotool type "nikto -h $host -port $port -no404 -maxtime 15m -Format htm --output $home/data/nikto/$host-$port.htm ; exit"
-        sleep 1
-        xdotool key Return
-    done < "$location"
-    ;;
+        while IFS=: read -r host port; do
+            xdotool key ctrl+shift+t
+            sleep 1
+            xdotool type "nikto -h $host -port $port -no404 -maxtime 15m -Format htm --output $HOME/data/nikto/$host-$port.htm ; exit"
+            sleep 1
+            xdotool key Return
+        done < "$location"
+        ;;
 
-    3) f_main;;
-    *) f_error;;
+    3)
+        f_main
+        ;;
+
+    *)
+        f_error
+        ;;
 esac
 
 echo
-echo $medium
+echo "$medium"
 echo
-echo "***Scan complete.***"
+echo "[*] Scan complete."
 echo
-echo
-echo -e "The new report is located at ${YELLOW}$home/data/nikto-$port/${NC}\n"
+echo -e "The new report is located at ${YELLOW}$HOME/data/nikto-$port/${NC}\n"
