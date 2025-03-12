2f5cdc33a30f6145b09c662d9f46635d30fe3732
Lee Baird <leebaird@gmail.com>
2025-03-12 09:08:57 -0500

    Updated install to handle Ubuntu.

--- config/install.sh
+++ config/install.sh
@@ -2,6 +2,11 @@
 
 cp tmux.conf "$HOME"/.tmux.conf
 cp vimrc "$HOME"/.vimrc
-cat zshrc >> "$HOME"/.zshrc
 
-source "$HOME"/.zshrc 2>/dev/null
+if grep -iq "kali" /etc/os-release; then
+    cat zshrc >> "$HOME"/.zshrc
+    source "$HOME"/.zshrc 2>/dev/null
+else
+    cp zshrc "$HOME"/.bash_aliases
+    source "$HOME"/.bash_aliases
+fi
