6466391c410613fd985d0e47bb6b733d43200e5c
Lee Baird <leebaird@gmail.com>
2026-05-05 08:21:17 -0500

    Had to add color variables back.

--- update.sh
+++ update.sh
@@ -2,6 +2,11 @@
 
 # by Lee Baird (@discoverscripts)
 
+# Variables
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 echo
 echo -e "${BLUE}Updating the operating system.${NC}"
 apt update ; apt -y upgrade ; apt -y dist-upgrade ; apt -y autoremove ; apt -y autoclean ; updatedb
